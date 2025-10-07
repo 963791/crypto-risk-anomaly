716792b16f4724afff946c554db728a9df07c92e
963791 <mayurigajbhiye0908@gmail.com>
2025-10-07 13:48:41 +0530

    Create urls.py

---
+++ config/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/accounts/", include("apps.accounts.urls")),
+    path("api/transactions/", include("apps.transactions.urls")),
+    path("api/ml/", include("apps.ml_engine.urls")),
+    path("", include("apps.dashboard.urls")),
+]
+
