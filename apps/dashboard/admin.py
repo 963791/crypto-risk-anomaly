93925aa908399f5cddea3869f8ce2c1abafb06d4
963791 <mayurigajbhiye0908@gmail.com>
2025-10-08 15:44:03 +0530

    Update admin.py

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import DashboardExample
 
-admin.site.register(DashboardExample)
+@admin.register(DashboardExample)
+class DashboardExampleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # only fields in model
+    list_filter = ('name',)  # optional
+    ordering = ('name',)
+
