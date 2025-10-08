b1b5296160c08ccb37dbe4d3369cad6e33014fd9
963791 <mayurigajbhiye0908@gmail.com>
2025-10-08 16:38:35 +0530

    Update admin.py

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -3,7 +3,4 @@ from .models import DashboardExample
 
 @admin.register(DashboardExample)
 class DashboardExampleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at')  # only fields in model
-    list_filter = ('name',)  # optional
-    ordering = ('name',)
-
+    list_display = ("title",)  # only include fields that actually exist
