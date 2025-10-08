679fcb09419a7539ebe6eda9d1e0e6c573ef74d6
963791 <mayurigajbhiye0908@gmail.com>
2025-10-08 15:44:43 +0530

    Update admin.py

--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -3,9 +3,18 @@ from .models import Transaction, Batch, WalletProfile
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("tx_hash","from_address","to_address","amount","timestamp","is_anomaly","risk_label")
-    search_fields = ("tx_hash","from_address","to_address")
-    list_filter = ("is_anomaly","risk_label")
+    list_display = ("tx_hash", "from_address", "to_address", "amount", "timestamp", "is_anomaly", "risk_label")
+    search_fields = ("tx_hash", "from_address", "to_address")
+    list_filter = ("is_anomaly", "risk_label")
 
-admin.site.register(Batch)
-admin.site.register(WalletProfile)
+@admin.register(Batch)
+class BatchAdmin(admin.ModelAdmin):
+    list_display = ("batch_name", "created")  # actual fields from your model
+    list_filter = ("batch_name",)
+
+@admin.register(WalletProfile)
+class WalletProfileAdmin(admin.ModelAdmin):
+    list_display = ("wallet_id", "balance")  # actual fields from your model
+    list_filter = ("balance",)
+    ordering = ("wallet_id",)
+
