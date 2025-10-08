9e0028a54215fc02d8791b7b40b6764e3dba90e3
963791 <mayurigajbhiye0908@gmail.com>
2025-10-08 16:56:37 +0530

    Update admin.py

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from .models import DashboardExample
 
-@admin.register(DashboardExample)
-class DashboardExampleAdmin(admin.ModelAdmin):
-    list_display = ("title",)  # only include fields that actually exist
-    search_fields = ("name",)           # only searchable fields that exist
-    list_filter = ()                     # only fields that exist
-    ordering = ("id",)                  # only fields that exist
+@admin.site.register(DashboardExample)
+
+   
 
